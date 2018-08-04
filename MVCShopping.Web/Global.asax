ad0a8046722b189c7aa16910b26bfcaf9dc74af3
yb <yangbo201423@163.com>
2018-08-04 20:33:24 +0800

    创建项目

---
+++ MVCShopping.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCShopping.Web.MvcApplication" Language="C#" %>
---
+++ MVCShopping.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCShopping.Web.Startup))]
+namespace MVCShopping.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
